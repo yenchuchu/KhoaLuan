db65ae3cc273c2f890cd2df7bb5806bdbbd96422
thuyhien <yenchuchu95@gmail.com>
2017-04-18 14:49:31 +0700

    cogfix login facebook

--- resources/views/dashboard/check-role-facebook.blade.php
+++ resources/views/dashboard/check-role-facebook.blade.php
@@ -20,6 +20,66 @@
             margin-bottom: 90px;
         }
 
+        .block-12 a>label:active,
+        .block-12 a>label:focus {
+            background-color: #00b1b3;
+            color: white !important;
+        }
+
+        .block-12 a:hover,
+        .block-12 a:focus {
+            text-decoration: none !important;
+        }
+
+        .block-12 .option .item {
+            float: left;
+            position: relative;
+        }
+
+        .block-12 .option .radio {
+            opacity: 0;
+            position: absolute;
+            top: 0;
+            left: 0;
+            visibility: hidden;
+        }
+
+        .block-12 input, select, textarea {
+            vertical-align: middle;
+            font-family: inherit;
+            font-size: inherit;
+            border: 0;
+            outline: 0;
+            color: #1b2e3a;
+        }
+
+        .block-12 .option .radio:checked ~ label {
+            background: #00b1b3;
+            color: #fff;
+        }
+
+        .block-12 .option .radio:checked ~ label>a {
+            color: white !important;
+        }
+
+        .block-12 .option .item:first-child label {
+            border-radius: 3px 0 0 3px;
+        }
+
+        .block-12 .option label {
+            display: block;
+            width: 110px;
+            height: 36px;
+            line-height: 36px;
+            text-align: center;
+            background: #eee;
+            cursor: pointer;
+        }
+
+        .block-12 .option .item {
+            float: left;
+            position: relative;
+        }
     </style>
 @stop
 
@@ -35,35 +95,32 @@
                         <label for="choose-object" class="col-md-4 control-label">Chọn đối tượng</label>
                         <div class="col-md-6 block-12">
                             <div class="option">
-                                <div class="group nav nav-tabs">
+                                <div class="group nav nav-tabs" >
                                     <div class="item objs">
                                         <input type="radio" id="student" name="office_type" value="ST"
-                                               class="radio"
-                                               checked="">
-                                        <label for="student"><a data-toggle="tab"
-                                                                href="#student-tabs">Student</a></label>
+                                               class="radio" checked>
+                                        <label id="label-stdent-id" for="student">STUDENT</label>
                                     </div>
-                                    <div class="item objt">
+                                    <div class="item objt" style="color: #00b1b3">
                                         <input type="radio" id="author" name="office_type" value="AT"
                                                class="radio">
-                                        <label for="teacher"><a data-toggle="tab"
-                                                                href="#teacher-tabs">Author</a></label>
+                                        <label id="label-author-id" for="author">AUTHOR</label>
                                     </div>
+                                    <input type="hidden" id="check_type_user">
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <div class="tab-content">
-                        <div id="teacher-tabs" class="tab-pane fade">
+                        <div id="teacher-tabs" class="tab-pane fade ">
                         </div>
                         <div id="student-tabs" class="tab-pane fade in active">
                             <div class="form-group">
                                 <label for="class" class="col-md-4 control-label">Class</label>
 
                                 <div class="col-md-6">
-                                    <select name="class" id="class-choose">
-                                        <option value="0">-- Chọn lớp --</option>
+                                    <select name="class" id="class">
                                         @foreach($classes as $class)
                                             <option value="{{$class->id}}">{{$class->title}}</option>
                                         @endforeach
@@ -72,7 +129,9 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
+
                 <div class="modal-footer">
                     <button type="submit" id="btn-done-loginfb" class="btn btn-default" data-dismiss="modal">Done</button>
                 </div>
@@ -83,3 +142,36 @@
         </div>
     </section>
 @stop
+
+@section('script')
+
+    <script>
+        $('#label-stdent-id').css('background-color', '#00b1b3');
+        $('#label-stdent-id').css('color', 'white');
+
+        $('#label-author-id').click(function () {
+            $(this).css('background-color', '#00b1b3');
+            $(this).css('color', 'white');
+
+            $('#label-stdent-id').css('background-color', '#eeeeee');
+            $('#label-stdent-id').css('color', '#00b1b3');
+
+            $('#student-tabs').hide();
+        });
+
+        $('#label-stdent-id').click(function () {
+            $(this).css('background-color', '#00b1b3');
+            $(this).css('color', 'white');
+
+            $('#label-author-id').css('background-color', '#eeeeee');
+            $('#label-author-id').css('color', '#00b1b3');
+
+            $('#student-tabs').show();
+        });
+
+        $( 'input[type="radio"]' ).on( "click", function() {
+            $( "#check_type_user" ).html( $( "input:checked" ).val() + " is checked!" );
+        });
+
+    </script>
+@endsection

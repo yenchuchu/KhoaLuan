547d7d583004da4ddd6c855a5b4764a626293063
thuyhien <yenchuchu95@gmail.com>
2017-05-04 15:16:59 +0700

    thay đổi giao diện trang chủ

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/redirect_login';
 
     /**
      * Create a new controller instance.

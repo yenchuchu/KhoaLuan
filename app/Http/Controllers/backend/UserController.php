e79fa20d27b4cafa74ec0ea7aced65537ccb6fa5
thuyhien <yenchuchu95@gmail.com>
2017-05-09 23:08:24 +0700

    change language

--- app/Http/Controllers/backend/UserController.php
+++ app/Http/Controllers/backend/UserController.php
@@ -70,7 +70,7 @@ class UserController extends Controller
     {
         $user_id = $request->all();
         $user = User::whereId($user_id)->with('roles', 'socials')->first();
-
+dd($user);
 //        if (count($user) != 1) {
 //            return response()->json([
 //                'code' => 404,
